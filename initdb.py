d6c1d53d0615fcbf20b379d058632bf881ffe649
rhavymaia <rmg3@cin.ufpe.br>
2025-10-30 16:36:09 -0300

    feat: Criação do banco com arquivo sql.

--- initdb.py
+++ initdb.py
@@ -2,38 +2,21 @@ import sqlite3
 
 DATABASE_NAME = "censoescolar.db"
 
-tables = [
-    """
-        CREATE TABLE IF NOT EXISTS tb_instituicao (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                codigo TEXT NOT NULL,
-                nome TEXT NOT NULL,
-                co_uf INTEGER NOT NULL,
-                co_municipio INTEGER NOT NULL,
-                qt_mat_bas INTEGER NOT NULL,
-                qt_mat_prof INTEGER NOT NULL,
-                qt_mat_esp INTEGER NOT NULL
-        )
-    """,
-    """
-        CREATE TABLE IF NOT EXISTS tb_usuario (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                nome TEXT NOT NULL,
-                cpf TEXT NOT NULL,
-                nascimento DATE NOT NULL
-        )
-    """
-]
-
 
 def create_tables():
     print("Iniciando criação")
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
-    for table in tables:
+
+    with open('schema.sql') as f:
         print("Criando as tabelas")
-        cursor.execute(table)
+        conn.executescript(f.read())
+
+    print("Inserindo usuário padrão")
+    cursor.execute("INSERT INTO tb_usuario (nome, cpf, nascimento) VALUES (?, ?, ?)",
+                   ('João da Silva', '00011122255', '2025-10-30'))
     conn.commit()
+
     print("Fechar conexão")
     conn.close()
 
---
+++ schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS tb_instituicao (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        codigo TEXT NOT NULL,
+        nome TEXT NOT NULL,
+        co_uf INTEGER NOT NULL,
+        co_municipio INTEGER NOT NULL,
+        qt_mat_bas INTEGER NOT NULL,
+        qt_mat_prof INTEGER NOT NULL,
+        qt_mat_esp INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tb_usuario (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        cpf TEXT NOT NULL,
+        nascimento DATE NOT NULL
+);
